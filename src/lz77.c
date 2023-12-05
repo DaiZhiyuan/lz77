a7a35eb6e27ac5cf4b454ba3ad77d5545a3c4f32
Daizhiyuan <daizhiyuan@phytium.com.cn>
2023-12-05 14:26:59 +0800

    core: Add a policy to automatically determine the compression level
    Signed-off-by: Daizhiyuan <daizhiyuan@phytium.com.cn>

--- src/lz77.c
+++ src/lz77.c
@@ -4,13 +4,32 @@
 
 #include "lz77.h"
 
-int lz77_compress(const void* input, int length, void* output) 
+int lz77_l1_compress(const void* input, int length, void* output)
 {
 	return 0;
 }
 
+int lz77_l2_compress(const void* input, int length, void* output)
+{
+	return 0;
+}
+
+int lz77_compress(const void* input, int length, void* output)
+{
+	if (length < 65536)
+		return lz77_l1_compress(input, length, output);
+
+	return lz77_l2_compress(input, length, output);
+}
+
 int lz77_compress_level(int level, const void* input, int length, void* output)
 {
+	if (1 == level)
+		return lz77_l1_compress(input, length, output);
+
+	if (2 == level)
+		return lz77_l2_compress(input, length, output);
+
 	return 0;
 }
 
