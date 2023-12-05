fb71210dd3e5ccee8ef7deb5b3151a7c498bb747
Daizhiyuan <daizhiyuan@phytium.com.cn>
2023-12-05 17:11:16 +0800

    utils: implementation mem{move,cpy,cmp} utils functions
    Signed-off-by: Daizhiyuan <daizhiyuan@phytium.com.cn>

--- src/lz77.c
+++ src/lz77.c
@@ -3,6 +3,8 @@
  */
 
 #include "lz77.h"
+#include <stdint.h>
+#include <string.h>
 
 /*
  * Give hints to the compiler for branch prediction optimization.
@@ -14,6 +16,56 @@
 #define likely(x)		(x)
 #define unlikely(x)		(x)
 #endif
+
+static void lz77_memmove(uint8_t* dest, const uint8_t* src, uint32_t count)
+{
+	if ((count > 4) && (dest >= src + count)) {
+		memmove(dest, src, count);
+	} else {
+		switch (count) {
+			default:
+				do {
+					*dest++ = *src++;
+				} while (--count);
+				break;
+			case 3:
+				*dest++ = *src++;
+			case 2:
+				*dest++ = *src++;
+			case 1:
+				*dest++ = *src++;
+			case 0:
+				break;
+		}
+	}
+}
+
+static void lz77_memcpy(uint8_t* dest, const uint8_t* src, uint32_t count)
+{
+	memcpy(dest, src, count);
+}
+
+static uint32_t lz77_readu32(const void* p)
+{
+	return *(const uint32_t*)p;
+}
+
+static uint32_t lz77_memcmp(const uint8_t* p, const uint8_t* q, const uint8_t* len)
+{
+	const uint8_t* start = p;
+
+	if (lz77_readu32(p) == lz77_readu32(q)) {
+		p += 4;
+		q += 4;
+	}
+
+	while (q < len) {
+		if (*p++ != *q++)
+			break;
+	}
+
+	return p - start;
+}
 
 int lz77_l1_compress(const void* input, int length, void* output)
 {
