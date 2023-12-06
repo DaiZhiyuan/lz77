fd0f958e1ecfcedacd7c9e7d3483277f5d9ee21f
Daizhiyuan <daizhiyuan@phytium.com.cn>
2023-12-06 13:16:25 +0800

    core: remove redundant function prototypes
    Signed-off-by: Daizhiyuan <daizhiyuan@phytium.com.cn>

--- include/lz77.h
+++ include/lz77.h
@@ -6,7 +6,6 @@
  #define __LZ77_H__
 
 int lz77_compress(const void* input, int length, void* output);
-int lz77_compress_level(int level, const void* input, int length, void* output);
 int lz77_decompress(const void* input, int length, void* output, int maxout);
 
  #endif
