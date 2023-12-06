14d542661a064a4d9919085d28f76e14cf7fd27d
Daizhiyuan <daizhiyuan@phytium.com.cn>
2023-12-06 09:55:10 +0800

    core: support only for L1 compression level tends to be faster
    Signed-off-by: Daizhiyuan <daizhiyuan@phytium.com.cn>

--- src/lz77.c
+++ src/lz77.c
@@ -103,32 +103,8 @@ static uint32_t lz77_memcmp(const uint8_t* p, const uint8_t* q, const uint8_t* l
 	return p - start;
 }
 
-int lz77_l1_compress(const void* input, int length, void* output)
-{
-	return 0;
-}
-
-int lz77_l2_compress(const void* input, int length, void* output)
-{
-	return 0;
-}
-
 int lz77_compress(const void* input, int length, void* output)
 {
-	if (length < 65536)
-		return lz77_l1_compress(input, length, output);
-
-	return lz77_l2_compress(input, length, output);
-}
-
-int lz77_compress_level(int level, const void* input, int length, void* output)
-{
-	if (1 == level)
-		return lz77_l1_compress(input, length, output);
-
-	if (2 == level)
-		return lz77_l2_compress(input, length, output);
-
 	return 0;
 }
 
